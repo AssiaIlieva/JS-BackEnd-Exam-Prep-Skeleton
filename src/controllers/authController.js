ee972c351cac18cd7a2b2a5626e589782120c731
AssiaIlieva <ac17@abv.bg>
2024-06-14 11:10:31 +0300

    Create user register functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 
+const authService = require('../services/authService')
+
 router.get('/register', (req, res) => {
     res.render('auth/register')
+});
+
+router.post('/register', async(req, res) => {
+    const userData = req.body;
+    if(req.body.password !== req.body.rePassword){
+       const message = `Password and Confirm Password doesn\'t match`;
+       res.render('auth/register', {...userData, error: message}) 
+    }
+    await authService.register(userData); 
+    res.redirect('/auth/login');
 })
 
 module.exports = router;

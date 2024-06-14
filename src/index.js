acea2167f3d395089a06c572e15e7ffaf312228d
AssiaIlieva <ac17@abv.bg>
2024-06-14 08:45:34 +0300

    Connect to the Database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const configExpress = require('./config/configExpress');
@@ -11,9 +12,12 @@ configHandlebars(app);
 
 app.use(routes)
 
+// TODO:Change the database
+mongoose.connect('mongodb://127.0.0.1:27017/course-book');
 
-
-
+mongoose.connection.on('connected', () => console.log('DB is connected'));
+mongoose.connection.on('disconnected', () => console.log('DB is disconnected'));
+mongoose.connection.on('error', (err) => console.log('err'));
 
 
 app.listen(5000, () => {

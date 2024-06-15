3e1d9c3c5f38fe55271418227a97347ae6d7813f
AssiaIlieva <ac17@abv.bg>
2024-06-15 17:38:58 +0300

    Fix dynamic navigation

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
-
+const {isAuth} = require('../middlewares/authMiddlware');
 
 router.get('/', (req, res) => {
     res.render('home')
 });
+
+router.get('/authorize-test', isAuth, (req, res) => {
+    console.log(req.user);
+    res.send('Your are authorized');
+})
 
 module.exports = router
